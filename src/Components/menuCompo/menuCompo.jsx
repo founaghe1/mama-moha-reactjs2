e866c6f4e34ab1a5c6696d3222e69ec8fa30fa73
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-05-20 18:38:42 +0000

    First ajout container

--- src/Components/menuCompo/menuCompo.jsx
+++ src/Components/menuCompo/menuCompo.jsx
@@ -3,7 +3,27 @@
 export const Menu = () =>{
     return(
         <>
-        <div className="menu">Coucou</div>
+        <div className="menu">
+            <div className="container-fuid">
+                <div className="row">
+                    <div className="col-lg-8 col-md-6 col-sm-12">
+                        <h1>Row1 Colone 1</h1>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <h1>Row1 Colone 2</h1>
+                    </div>
+                </div>
+                {/* row 2 */}
+                <div className="row">
+                    <div className="col-lg-8 col-md-6 col-sm-12">
+                        <h1>Row2 Colone 1</h1>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <h1>Row2 Colone 2</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 };
