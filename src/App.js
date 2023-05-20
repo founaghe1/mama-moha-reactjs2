afe9aaa38ba1cda963373bc3c2044c5530ba637a
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-05-20 18:28:09 +0000

    First add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Menu } from './Components/menuCompo/menuCompo';
 
 function App() {
   return (
     <div className="App">
       <h1>Bonjour</h1>
+      <Menu />
     </div>
   );
 }
